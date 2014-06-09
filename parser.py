dc1511a670ec1a414cd6c89a3b26bcddaa1b3849
barrtos <barrtos@gmail.com>
2014-06-09 12:23:53 +0200

    weather feedback for desired city ready

--- parser.py
+++ parser.py
@@ -7,14 +7,12 @@ import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
 def get_par():
-
-	print 'Weather for a good start!\n'
-	#user = raw_input('User name (ex. Msitake): ')
+	q = raw_input('Where are you? ')
 	#limit = int(input('Numer of tracks (1-10): '))
 	query_params={
-		'q':'Krakow',
+		'q':q,
 		'units':'metric'}
-	return query_params, limit, user
+	return query_params
 
 def get_dat(ppp):
 	endpoint = 'http://api.openweathermap.org/data/2.5/weather/'
@@ -22,21 +20,14 @@ def get_dat(ppp):
 	data = response.json()
 	return data
 	
-parameters, li, user= get_par()
+def print_weather():
+	os.system('cls' if os.name == 'nt' else 'clear')
+	print "Weather for a good start!\n"
+	pprint.pprint(data)
+	print "Good morning %s!" %(data['name'])
+	print data['main']['temp']
+	
+parameters=get_par()
 data = get_dat(parameters)
-
-os.system('cls' if os.name == 'nt' else 'clear')
-print "Best %s tracks for user %s overall! \n" %(li, user)
-track = []
-#pprint.pprint(data)
-
-	
-with open("results.html", "w") as textfile:
-	textfile.write("<!DOCTYPE html><html><head><link type=\"text/css\" rel=\"stylesheet\" href=\"stylesheet.css\"/><title>LFM Track Grab</title></head><body><div><img src=\"http://www.justinparks.com/wp-content/uploads/2009/10/lastfm-logo-square-webtreatsetc.png\"/><p><h2>Last.fm top ")
-	textfile.write(str(li)) 
-	textfile.write(" tracks from user: ")
-	textfile.write(str(user)) 
-	textfile.write("!</h2></p>")
-	
-	textfile.write("<br/><br/></div></body></html>")
+print_weather()
 
