7d05483efa235523abf15ecde2af1e9c9c9451f6
barrtos <barrtos@gmail.com>
2014-06-10 19:48:27 +0200

    experimental update

--- parser2.py
+++ parser2.py
@@ -33,19 +33,19 @@ data = get_dat(par)
 print_weather()
 
 
-with open("results.html", "w") as textfile:
+with open("results.php", "w") as textfile:
 
 #Html init
-	textfile.write("<!DOCTYPE html><html><head><link type=\"text/css\" rel=\"stylesheet\" href=\"stylesheet.css\"/><title>GooD'ay</title></head><body>")
+	textfile.write("<html><head><link type=\"text/css\" rel=\"stylesheet\" href=\"stylesheet.css\"/><title>GooD'ay</title></head><body>")
 	
 #Image	
-	textfile.write("<div><img src=\"http://www.justinparks.com/wp-content/uploads/2009/10/lastfm-logo-square-webtreatsetc.png\"/><p><h2>Last.fm top ")
+	#textfile.write("<div><img src=\"http://www.justinparks.com/wp-content/uploads/2009/10/lastfm-logo-square-webtreatsetc.png\"/><p><h2>Last.fm top ")
 	
 #Grooveshark widget
-	textfile.write('<object width="250" height="250" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" id="gsPlaylist9858105448" name="gsPlaylist9858105448"><param name="movie" value="http://grooveshark.com/widget.swf" /><param name="wmode" value="window" /><param name="allowScriptAccess" value="always" /><param name="flashvars" value="hostname=grooveshark.com&playlistID=98581054&p=1&bbg=000000&bth=000000&pfg=000000&lfg=000000&bt=FFFFFF&pbg=FFFFFF&pfgh=FFFFFF&si=FFFFFF&lbg=FFFFFF&lfgh=FFFFFF&sb=FFFFFF&bfg=666666&pbgh=666666&lbgh=666666&sbh=666666" /><object type="application/x-shockwave-flash" data="http://grooveshark.com/widget.swf" width="250" height="250"><param name="wmode" value="window" /><param name="allowScriptAccess" value="always" /><param name="flashvars" value="hostname=grooveshark.com&playlistID=98581054&p=1&bbg=000000&bth=000000&pfg=000000&lfg=000000&bt=FFFFFF&pbg=FFFFFF&pfgh=FFFFFF&si=FFFFFF&lbg=FFFFFF&lfgh=FFFFFF&sb=FFFFFF&bfg=666666&pbgh=666666&lbgh=666666&sbh=666666" /><span><a href="http://grooveshark.com/search/playlist?q=z.morning%20Kestrel" title="z.morning by Kestrel on Grooveshark">z.morning by Kestrel on Grooveshark</a></span></object></object>')
+	#textfile.write('<object width="250" height="250" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" id="gsPlaylist9858105448" name="gsPlaylist9858105448"><param name="movie" value="http://grooveshark.com/widget.swf" /><param name="wmode" value="window" /><param name="allowScriptAccess" value="always" /><param name="flashvars" value="hostname=grooveshark.com&playlistID=98581054&p=1&bbg=000000&bth=000000&pfg=000000&lfg=000000&bt=FFFFFF&pbg=FFFFFF&pfgh=FFFFFF&si=FFFFFF&lbg=FFFFFF&lfgh=FFFFFF&sb=FFFFFF&bfg=666666&pbgh=666666&lbgh=666666&sbh=666666" /><object type="application/x-shockwave-flash" data="http://grooveshark.com/widget.swf" width="250" height="250"><param name="wmode" value="window" /><param name="allowScriptAccess" value="always" /><param name="flashvars" value="hostname=grooveshark.com&playlistID=98581054&p=1&bbg=000000&bth=000000&pfg=000000&lfg=000000&bt=FFFFFF&pbg=FFFFFF&pfgh=FFFFFF&si=FFFFFF&lbg=FFFFFF&lfgh=FFFFFF&sb=FFFFFF&bfg=666666&pbgh=666666&lbgh=666666&sbh=666666" /><span><a href="http://grooveshark.com/search/playlist?q=z.morning%20Kestrel" title="z.morning by Kestrel on Grooveshark">z.morning by Kestrel on Grooveshark</a></span></object></object>')
 	
 #City input form
-	textfile.write('<form>User 1: <input type="text" name="name1"><br><input type="submit" value="Submit"></form>')
+	textfile.write('<form>Personalize your good day! :)<p>Where are you: <input type="text" name="city"><br><input type="submit" value="GooD\'ay!"></form>')
+	textfile.write("<?php $f = fopen(\"user_data.txt\", \"w\");fwrite($f, $_POST[\"city\"]); fclose($f);$f = fopen(\"user_data.txt\", "r");echo fgets($f); fclose($f);?>")
 	textfile.write("</p>")
 	textfile.write("<br/><br/></div></body></html>")
-
