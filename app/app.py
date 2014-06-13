4acab5e1ae069d82732a3a3348595d56806745a5
barrtos <barrtos@gmail.com>
2014-06-13 02:00:54 +0200

    big update, weather api integrated & fully functional

--- app/app.py
+++ app/app.py
@@ -1,8 +1,29 @@
+# -*- coding: utf-8 -*-
 import web
+import requests
+import pprint
+import os
+
+
+#Weather API
+
+def get_par(city):
+	query_params={'q':city,'units':'metric'}
+	return query_params
+
+def get_dat(ppp):
+	endpoint = 'http://api.openweathermap.org/data/2.5/weather/'
+	response = requests.get(endpoint, params=ppp)
+	data = response.json()
+	return data
 
 urls = (
   '/hello', 'Index'
 )
+
+
+
+#Rendering Index page
 
 app = web.application(urls, globals())
 
@@ -15,7 +36,12 @@ class Index(object):
     def POST(self):
         form = web.input(name="Nobody", greet="Hello")
         greeting = "GooD'ay! Good morning %s, good morning %s!" %(form.name, form.city)
-        return render.index(greeting = greeting)
+        temperature = "%s °C" %(get_dat(get_par(form.city))['main']['temp'])
+        desc = "%s today" %(get_dat(get_par(form.city))['weather'][0]['description'])
+        return render.index(greeting = greeting, temperature = temperature, desc = desc)
 
 if __name__ == "__main__":
     app.run()
+    
+    
+
